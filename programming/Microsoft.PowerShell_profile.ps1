15ce8e190eba2d621c16e5ed9beeb359e9728e28
Michael <michael.hixon@hotmail.com>
2022-10-25 22:08:15 -0600

    Update Microsoft.PowerShell_profile.ps1

--- programming/Microsoft.PowerShell_profile.ps1
+++ programming/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,19 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme Paradox
-Set-Location D:\Documents\
-$hosts = "$env:windir\System32\drivers\etc\hosts"
-Set-alias -Name n -Value nvim
+# Useful Vars
+$HOSTS = "$env:windir\System32\drivers\etc\hosts"
+$DOTSSHDIR = "$env:USERPROFILE\.ssh"
+$DOTCONFIGDIR = "$env:USERPROFILE\.config"
+$NVIMRC = "$env:LOCALAPPDATA\nvim\init.vim"
+
+# Alias'
+Set-Alias -Name n -Value nvim
+Set-Alias -Name ls -Value exa
+
+Function l {exa -lh}
+Function la {exa -a}
+Function lla {exa -lha}
+Function lt {exa --tree --git-ignore}
+Function fz {nvim $(fzf)}
+
+# Start Starship
+Invoke-Expression (&starship init powershell)
+#$ENV:STARSHIP_CONFIG = "$HOME\Documents\starship.toml"

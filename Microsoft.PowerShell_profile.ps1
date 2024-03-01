adcb438e52f7ac83c3a2d1dc1070189c90160fe7
Michael <michael.hixon@hotmail.com>
2024-02-29 19:42:11 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,24 +2,25 @@
 $HOSTS = "$env:windir\System32\drivers\etc\hosts"
 $DOTSSHDIR = "$env:USERPROFILE\.ssh"
 $DOTCONFIGDIR = "$env:USERPROFILE\.config"
-$NVIMRC = "$env:LOCALAPPDATA\nvim\init.vim"
+$NVIMRC = "$env:LOCALAPPDATA\nvim\"
 
-# Alias'
+#Alias'
 Set-Alias -Name n -Value nvim
-Set-Alias -Name ls -Value eza
 
+# eza helpers
+Set-Alias -Name ls -Value eza
 Function l {eza -lh}
 Function la {eza -a}
 Function lla {eza -lha}
 Function lt {eza --tree --git-ignore}
 Function fz {nvim $(fzf)}
 
-# Start Starship
+# Starship prompt
 Invoke-Expression (&starship init powershell)
-#$ENV:STARSHIP_CONFIG = "$HOME\Documents\starship.toml"
+$ENV:STARSHIP_CONFIG = "$HOME\Documents\starship.toml"
 
-# Start zoxide
-Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+# Zoxide init
+Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
 
 # scans the cwd for files older than the provide date
 function Get-Old-Files {

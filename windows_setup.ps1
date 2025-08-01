51cffb2b4c4630f736c543acd5325a8cc5eb9c59
Michael Hixon <michael.hixon@hotmail.com>
2025-08-01 00:19:47 -0600

    reoganize

---
+++ windows_setup.ps1
@@ -0,0 +1,17 @@
+# startup items
+
+$startupFolder = [Environment]::GetFolderPath('Startup')
+function makeShortcut {
+  param(
+    [String] $shortcutPath
+    [String] $target
+  )
+  $WshShell = New-Object -ComObject WScript.Shell
+  $Shortcut = $WshShell.CreateShortcut($shortcutPath)
+  $Shortcut.TargetPath = $target
+  $Shortcut.Save()
+}
+# main.ahk
+makeShortcut "$startupFolder\main.ahk.lnk" "$HOME\.Autohotkey\main.ahk"
+# glazewm
+makeShortcut "$startupFolder\glazewm.exe.lnk" "C:\Program Files\glzr.io\GlazeWM\cli\glazewm.exe"

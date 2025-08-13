7f836321b364b8797893836d3148e4cbd90a686a
Michael Hixon <michael.hixon@hotmail.com>
2025-08-13 17:06:47 -0600

    add junction creation for lazyvim

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -15,3 +15,6 @@ function makeShortcut {
 makeShortcut "$startupFolder\main.ahk.lnk" "$HOME\.Autohotkey\main.ahk"
 # glazewm
 makeShortcut "$startupFolder\glazewm.exe.lnk" "C:\Program Files\glzr.io\GlazeWM\cli\glazewm.exe"
+
+
+New-Item -ItemType Junction -Path "$env:LOCALAPPDATA\nvim" -Target C:\Users\mhixon\.config\nvim\

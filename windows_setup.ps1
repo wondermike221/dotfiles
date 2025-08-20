dacd3614b60634b40eaed9e21146db1c04d7a597
Michael Hixon <michael.hixon@hotmail.com>
2025-08-20 00:53:05 -0600

    fix issues with idempotency

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -11,17 +11,17 @@ function makeShortcut {
   $Shortcut.TargetPath = $target
   $Shortcut.Save()
 }
-if (-not (Test-Path "$startupFolder\main.ahk.lnk") -and Test-Path "$HOME\.Autohotkey\main.ahk") {
+if (-not (Test-Path "$startupFolder\main.ahk.lnk") -and (Test-Path "$HOME\.Autohotkey\main.ahk")) {
   # main.ahk
   makeShortcut "$startupFolder\main.ahk.lnk" "$HOME\.Autohotkey\main.ahk"
 }
-if (-not (Test-Path "$startupFolder\glazewm.exe.lnk") -and Test-Path "C:\Program Files\glzr.io\GlazeWM\cli\glazewm.exe") {
+if (-not (Test-Path "$startupFolder\glazewm.exe.lnk") -and (Test-Path "C:\Program Files\glzr.io\GlazeWM\cli\glazewm.exe")) {
   # glazewm
   makeShortcut "$startupFolder\glazewm.exe.lnk" "C:\Program Files\glzr.io\GlazeWM\cli\glazewm.exe"
 }
 
 # Set up EDITOR env
-if (where.exe nvim) {
+if (where.exe nvim -and (-not (Test-Path Env:EDITOR) -and ($env:EDITOR -ne "C:\Program Files/Neovim/bin/nvim.exe"))) {
   [Environment]::SetEnvironmentVariable
      ("EDITOR", (where.exe nvim), [System.EnvironmentVariableTarget]::User)
 } else {
@@ -29,8 +29,12 @@ if (where.exe nvim) {
 }
 
 # set up lazyvim
-New-Item -ItemType Junction -Path "$env:LOCALAPPDATA\nvim" -Target C:\Users\mhixon\.config\nvim\
+if (Test-Path "~\.config\nvim\") {
+  New-Item -ItemType Junction -Path "$env:LOCALAPPDATA\nvim" -Target C:\Users\mhixon\.config\nvim\
+}
 
 # set up alternate yazi config Path
-[Environment]::SetEnvironmentVariable
-  ("YAZI_CONFIG_HOME", "~/.config/yazi", [System.EnvironmentVariableTarget]::User)
+if ((Test-Path Env:YAZI_CONFIG_HOME) -and ($env:YAZI_CONFIG_HOME -ne "~\.config\yazi")) {
+  [Environment]::SetEnvironmentVariable
+    ("YAZI_CONFIG_HOME", "~\.config\yazi", [System.EnvironmentVariableTarget]::User)
+}
